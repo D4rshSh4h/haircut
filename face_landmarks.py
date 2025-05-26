ba8a0c4a5bff92ceb56050730ac9b8c205b81ca5
D4rshSh4h <sdarsh417@icloud.com>
2025-05-26 08:05:04 +0300

    webcam added

--- face_landmarks.py
+++ face_landmarks.py
@@ -5,19 +5,21 @@ import matplotlib.pyplot as plt
 
 DLIB_LANDMARK_MODEL_PATH = "C:\\Users\\USER\Desktop\\Code\\Datasets\\dlib_face_68\\shape_predictor_68_face_landmarks.dat"
 image_path = "C:\\Users\\USER\\Downloads\\square_face.jpg"
+image_name = "Webcam"
 
 def capture_pic():
     camport = 0
     cam = cv2.VideoCapture(camport)
     result, image = cam.read()
     if result:
-        cv2.imshow("Webcam", image)
+        cv2.imshow(image_name, image)
+        cv2.imwrite(image_name+".png", image)
         cv2.waitKey(0)
-        cv2.destroyWindow("Weebcam")
+        cv2.destroyWindow(image_name)
+
+def get_image():
+    pass
+
 
 if __name__ == "__main__":
-    while True:
-
-        if(input("") == "Y"):
-            capture_pic()
-            pass
+    capture_pic()
